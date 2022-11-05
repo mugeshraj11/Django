16f96f12f34fce92b828c2b5425c5f973482cd3c
MugeshRaj11 <97274900+MugeshRaj11@users.noreply.github.com>
2022-11-05 18:45:18 +0530

    Add files via upload

---
+++ project/app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path("",views.InsertPageView,name="insertpage"),
+    path("insert/",views.InsertData,name="insert"),
+    path("showpage/",views.ShowData,name="show"),
+]
